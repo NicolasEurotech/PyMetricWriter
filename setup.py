61966638a57dace85cb7f10f6cbeb96239937dcb
Nicolas Marcuzzi <nicolas.marcuzzi@eurotech.com>
2024-04-05 18:24:25 +0200

    update setup

--- setup.py
+++ setup.py
@@ -3,21 +3,23 @@ from setuptools import setup, find_packages
 setup(
     name='MetricWriterPy',
     version='0.1.1',
-    description='A simple Python library',
+    description='A metrci writer for prometheus in python.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/NicolasEurotech/MetricWriterPy.git',  # Replace with the URL of your GitHub repository
-    author='Your Name',
-    author_email='your@email.com',
-    license='MIT',  # Choose an appropriate license for your library
+    author='Nicolas Marcuzzi',
+    author_email='marcuzzi.nicolas.95@gmail.com',
+    license='MIT',  
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 1 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.10.6',
     ],
-    keywords='mwtric writer prometheus',
-    packages=find_packages(),
+    keywords='metric writer prometheus',
+    packages=find_packages(
+        include=['MetricWriterPy', 'MetricWriterPy.*']
+    ),
     python_requires='>=3.6',
-    install_requires=[],  # Add any dependencies your library requires
+    install_requires=[]
 )
